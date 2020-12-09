9215a79549285187a19dc95aed4f0b4474e512dd
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2020-12-09 14:57:00 -0500

    script.js
    I am using whitespeace to make it readable.

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 console.log("script");
 function initalquiz() {
 var timeleft = 0;
+   
+  //Declaring variables currentquestion and score with an ID element, along with mytarget.addEventListeners.    
+ var currentquestion = document.getElementById("id-of-dom-item");
+currentquestion.addEventListener("click",currentquestion)
+var score = document.getElementById("id-of-dom-item");  
+ score.addEventListener("click",score)     
     
 //Declare the variables for Code Quiz-This is the puescode
-var cosmiccodequiz  = document.getElementById("cosmic code quiz");
-var currentQuestion = document.getElementById("1");
-var score = document.getElementById("0");
+var cosmiccodequiz  = document.getElementById("cosmic code quiz");                                
 var questionGoSubmit = document.getElementById("results");
 var timeleft = document.getElementById("timeleft");
 var challengequestions = document.getElementById("challengequestions.length");
@@ -19,18 +23,24 @@ var beginquiz = document.getElementById("begin");
 var highScores =[]
  if (JSON.parse(localStorage.getItem('scores')) !== null) {
   highScores = JSON.parse(localStorage.getItem("scores"));
-        }
+ }
+    
 //Declaring time interval variables
+var currentquestion =1;
 var score = 0;
 var secondsLeft = 75;
 var currentindex = 0;
 }
-// Formed a start button function and declared startbutton variable with an ID element
+
+// Formed a startbutton and submitbutton function, declaring variables with an ID element
 function startbutton() {
 var startbutton = document.getElementById("id-of-dom-item");
+  startbutton.addEventListener("click",startbutton)
 }
+
 function submitbutton() {
  var submitbutton = document.getElementById("id-of-dom-item");
+  submitbutton.addEventListener("click",submitbutton)
 }
 
 //Formed a startquiz function
@@ -42,9 +52,9 @@ var column = null;
 var header = null;
 var button = null;
   cosmiccodequiz.setAttribute("class","container");
-
  timeleft=challengequestions * 15;
  timeleft.setAttribute("value",timeleft);
+ 
  //  Method for stopping the interval 
   //Formed a setInterval function
   var myInterval = setInterval(function() {
@@ -105,9 +115,9 @@ row.append(column2);
 
 button = document.createElement("button");
 button.setAttribute("class","btn btn-primary");
-  button.setAttribute("type","button");
- button.innerHTML = questions[currentQuestion-1].choices[i];
-  column2.append(button);
+ button.setAttribute("type","button");
+ button.innerHTML = questions[currentquestion-1].choices[i];
+ column2.append(button);
  button.addEventListener("click",function(){
  //  When the user clicks one of the answer buttons, then it would have a display message "Yes! You got it!" If incorrect, then the seconds would be deducted 
  if (clickTimeout) {
@@ -126,18 +136,19 @@ row.append(column);
 button = document.createElement("button");
  button.setAttribute("class","btn btn-primary");
  button.setAttribute("type","button");
- button.innerHTML = questions[currentQuestion-1].choices[i];
+ button.innerHTML = questions[currentquestion-1].choices[i];
   column.append(button);
  button.addEventListener("click",function(){
+ 
  //  When the user clicks the anwers buttons, the message is displayed. If it is correct,  15 seconds is deducted from the timer
   if (clickTimeout) {
   return;
-   }
+   }   
 clickTimeOut = true;
   clearInterval(myInterval);
   var column = cosmiccodequiz.children[0].children[1];
   var row = document.createElement("div");
-  row.setAttribute("class","row border-top");
+ row.setAttribute("class","row border-top");
  column.append(row);
 
 column = document.createElement("div");
@@ -155,20 +166,21 @@ if (this.innerHTML === questions[currentQuestion - 1].answer) {
  timeleft = 0;
  }}}
  ). timeleft.setAttribute("value", timeleft);
-  currentQuestion++;
-  if (currentQuestion>challengequestions.length) {
+  currentquestion++;
+  if (currentquestion>challengequestions.length) {
  score = timeleft;
   }
-  //Formed a TimeOut function
+ 
+ //Formed a TimeOut function
   TimeOut (function() {
  // When the answer is selected, it moves on to new question after timer hits 2 seconds
-  if (currentQuestion>challengequestions) {
+  if (currentquestion>challengequestions) {
   //  results
    cosmiccodequiz.setAttribute("class","container d-none");
 submitbutton.setAttribute("class","container");
   score.setAttribute("value",score);
  } else {
- CreateNewQuestion(currentQuestion);
+ CreateNewQuestion(currentquestion);
 TimeOut = false;
  myInterval = setInterval(function() {
     if (timeleft<1) {
@@ -184,6 +196,7 @@ TimeOut = false;
  },2000);
 });
 }}
+
 //Formed a function saveHighScore
 function saveHighScore() {
     var initials = document.getElementById("initials-entry");
@@ -196,7 +209,7 @@ function saveHighScore() {
  console.log(highScores);
  localStorage.setItem("scores",JSON.stringify(highScores));
 }
-CreateNewQuestion(currentQuestion);
+CreateNewQuestion(currentquestion);
 var submitbutton = document.getElementById("submit-initials");
 
 cosmiccodequiz.setAttribute("class","container d-none");
@@ -216,7 +229,6 @@ for (i=0; i<Highscores.length; i++) {
 paragraph.innerHTML = "Initials: " + highScores[i].initials + "   Score: " + highScores[i].highScore;
  column2.append(paragraph);
   
-
 // Formed add.Eventlisteners at the bottom of my code
 submitbutton.addEventListener("click",submit-initials);
 startbutton.addEventListener("click",startbutton);
