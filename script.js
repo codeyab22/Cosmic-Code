cdd7f95a8f2b5e705f7b3f275003c29381faa0fb
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-01-12 14:01:24 -0500

    script.js
    This . needs to be replaced with #

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     console.log("Next Question...");
     });
     
-    $(".highscoreButton").on("click", function () {
+    $("#highscoreButton").on("click", function () {
     console.log("Scores revealed!");
    });
 
